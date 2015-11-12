f6be65cb5e7428145cac308f1041f04a506109c1
miles74 <milliecollado@yahoo.com>
2015-11-12 20:23:45 +1300

    12-11-2015 8:23 pm Petshopmvc

---
+++ Petshopmvc/Petshopmvc/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DHTMLX.Scheduler;
+
+namespace Petshopmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var scheduler = new DHXScheduler(this);
+            scheduler.Skin = DHXScheduler.Skins.Flat;
+            return View(scheduler);
+        }
+
+    }
+}
